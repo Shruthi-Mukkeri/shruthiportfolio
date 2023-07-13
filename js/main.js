e196d69a782feeeff563c0d289e2ad6999913ff0
Mukkeri Shruthi <mukkerishruthi31@gmail.com>
2023-07-13 11:18:50 +0530

    Changes in navbar functionality.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 const collapsibles = document.querySelectorAll(".collapsible");
+const heroPage = document.querySelector(".home-hero");
+const nav = document.querySelector(".nav");
+
 collapsibles.forEach((item) =>
-  item.addEventListener("click", function (){
+  item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
-  }) 
+  })
 );
+
+const sticky = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(sticky, {
+  root: null,
+  threshold: 0,
+});
+
+headerObserver.observe(heroPage);
